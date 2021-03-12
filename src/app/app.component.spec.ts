7f91bf8f11bbbe54f811651085cd6c3d43455ba2
Eloy Gómez <eloy@escenit.com>
2021-03-12 20:52:44 +0100

    GH pages

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'escenitech'`, () => {
+  it(`should have as title 'escenit'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('escenitech');
+    expect(app.title).toEqual('escenit');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to escenitech!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to escenit!');
   });
 });
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 const packageJson = require('../../package.json');
 
 export const environment = {
-  appName: 'Escenitech',
+  appName: 'escenit',
   owner: 'Escenit Media S.L.',
   envName: 'PROD',
   themeName: 'default-theme',
